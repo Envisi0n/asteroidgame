60c3dae52409a3a17bfb00c66991d23f5c081e3a
Cam <cmorris@cs.uml.edu>
2012-02-09 22:34:03 -0500

    THINGS

---
+++ asteroidgame/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Cameron Morris - Cameron_Morris@student.uml.edu
+ *
+ * Created on February 9, 2012, 10:26 PM
+ */
+
+#include <cstdlib>
+
+using namespace std ;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0 ;
+}
+
